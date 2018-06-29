e3f51fc411e14c005bc9f3c78af6554888e4c9f8
Kim Minho <minmin0416@naver.com>
2018-06-29 11:29:47 +0900

    Module: Add npm cors module.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// cors
+const cors = require('cors');
+
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -17,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 
