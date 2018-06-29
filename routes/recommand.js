0d8fd889dc855d4ec17e0bfcb27e57948f58f310
Kim Minho <minmin0416@naver.com>
2018-06-30 00:04:01 +0900

    Module: Add exec ffmpeg submodule.

--- routes/recommand.js
+++ routes/recommand.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 // mongoose
 const TagSchema = require('../models/Tag');
 
+router.get('/', function(req, res, next) {
+
+});
 
 router.post('/music', function (req, res, next) {
     let time = new Date();
@@ -25,12 +29,24 @@ router.post('/music', function (req, res, next) {
     });
 
     Promise.all(promiseAllArr)
-        .then(function(val) {
-            console.log(' POST /recommand/music : Promise ', val, " -  ", time);
-            console.log("POST /recommand/music :" + "resultMapObj: ", resultMapObj);
-            let top = topkObj(resultMapObj);
-            res.json(top);
-        });
+    .then(function(val) {
+        console.log(' POST /recommand/music : Promise ', val, " -  ", time);
+        console.log("POST /recommand/music :" + "resultMapObj: ", resultMapObj);
+        let top = topkObj(resultMapObj);
+
+        let ffconcat = fs.createWriteStream('in.ffmpeg');
+        ffconcat.write('ffconcat version 1.0\n' +
+            'file image01.png\n' +
+            'duration 3\n' +
+            'file image02.png\n' +
+            'duration 5\n' +
+            'file image03.png');
+        ffconcat.end();
+        exec('ffmpeg -i in.ffmpeg -i ./' + top[0].filename + ' -c:a copy -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" out.mp4', function(err, stdout, stderr) {
+            console.log("Stdout: ", stdout);
+            res.send('ok');
+        })
+    });
 });
 
 function topkObj(obj, k) {
