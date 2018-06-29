14ee617375613e23a3c6d0418a0e14a368194aed
Kim Minho <minmin0416@naver.com>
2018-06-29 10:40:08 +0900

    Module: Add multer module and add uploads router.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// mongoose
+
+
+// multer
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destinamtion: function(req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function(req, file, cb) {
+        let filename = req.body.filename;
+        cb(null, filename);
+    }
+});
+const upload = multer({
+    storage: storage
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/upload', function(req, res, next) {
+   res.render('upload');
+});
+
+router.post('/upload', upload.single('soundFile'), function(req, res, next) {
+    let time = new Date();
+    let specific = req.body.specific;
+    let dst = req.file.destination;
+
+
+    console.log(time, " POST /uploads : ", req.file, " are uploads in ", req.file.dest)
+
+    res.send("success");
 });
 
 module.exports = router;
