5f01e5944783302c25377fedf8e957b3007a974c
Kim Minho <minmin0416@naver.com>
2018-06-30 03:56:06 +0900

    Fix syntax

--- routes/recommand.js
+++ routes/recommand.js
@@ -92,7 +92,7 @@ function execffmpeg(topAudioList, photoCnt, callback) {
         if (err) console.log("execffmpeg: ", err);
         let query = "";
         if(topAudioList[0] === undefined) {
-            query = 'ffmpeg -i in.ffconcat -i ' + workDir +'/'+ '1530260870330Get_Outside\ \(mp3cut.net\).mp3' + ' -c:a copy -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" out.mp4';
+            query = 'ffmpeg -i in.ffconcat -i ' + workDir +'/'+ '1530243265582Get_Outside\\ \\(mp3cut.net\\).mp3' + ' -c:a copy -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" out.mp4';
         } else {
             query = 'ffmpeg -i in.ffconcat -i ' + workDir +'/'+ topAudioList[0].filename + ' -c:a copy -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" out.mp4';
         }
